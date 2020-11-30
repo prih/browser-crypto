7e595bef194ab581d330d99330edfec7adab9b38
prih <prih@prihhost.com>
2020-11-30 21:25:31 +0200

    add secp256k1

--- index.src.js
+++ index.src.js
@@ -3,12 +3,14 @@ var bip39;
 var bitcoinjs;
 var ethereumjs;
 var ethereumjsTx;
+var secp256k1;
 
 crypto = require('crypto');
 bip39 = require('bip39');
 bitcoinjs = require('bitcoinjs-lib');
 ethereumjs = require('ethereumjs-util');
 ethereumjsTx = require('ethereumjs-tx');
+secp256k1 = require('tiny-secp256k1');
 
 exports.crypto = crypto;
 exports.bip39 = bip39;
@@ -16,3 +18,4 @@ exports.bitcoinjs = bitcoinjs;
 exports.bip32 = bitcoinjs.bip32;
 exports.ethereumjs = ethereumjs;
 exports.ethereumjsTx = ethereumjsTx;
+exports.secp256k1 = secp256k1;
